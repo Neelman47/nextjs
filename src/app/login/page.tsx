01db2230fe63ba0bc02346c5b68139ddb1dcd338
Neelman47 <neelshah707@gmail.com>
2025-03-14 23:21:20 +0530

    floating navbar component added from aceternity

---
+++ src/app/login/page.tsx
@@ -0,0 +1,8 @@
+
+export default function login() {
+    return (
+      <>
+      <h1 className="text-2xl text-center mt-2 text-blue-950 ">login</h1>
+      </>
+    );
+  }
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,8 @@
+
+export default function signup() {
+    return (
+      <>
+      <h1 className="text-2xl text-center mt-2 text-blue-950 ">signup</h1>
+      </>
+    );
+  }
