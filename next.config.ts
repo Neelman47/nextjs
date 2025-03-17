a5c0f5ac0528d76a99af7b3639ab7f51a87f0cc2
Neelman47 <neelshah707@gmail.com>
2025-03-17 22:02:59 +0530

    animated tooltip component added

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // add trusted sources here then external images will be used (allow these domain)
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import BackgroundBeamsWithCollision from '@/components/BackgroundBeamsWithCollision'
+import ATooltip from '@/components/ATooltip'
 
 export default function Home() {
   return (
     <>
     <BackgroundBeamsWithCollision/>
-    <h1 className="text-2xl text-center mt-9 text-blue-950 ">Main page</h1>
+    <h1 className="text-2xl text-center mt-9 text-blue-950 p-3">Main page</h1>
+    <ATooltip />
     <p>Lorem ipsum, minima nulla suscipit accusamus porro quas odio esse voluptates quibusdam natus. Esse eius neque facere a quia debitis sint dolorum hic autem, assumenda illo odio ducimus modi, numquam in nemo consequuntur quod optio recusandae deleniti quaerat est officia quidem? Distinctio voluptas facere repudiandae sint placeat? Ipsum impedit voluptatum nobis ducimus, corporis ullam repellat eos! Necessitatibus esse dolores ab porro debitis, iure ea consequatur reprehenderit maiores eligendi nisi at, aut accusantium dolore. Velit in incidunt recusandae eaque placeat illum deleniti quisquam praesentium soluta, similique qui cum eum! Deserunt officiis odio, dolor similique totam sapiente consequatur velit, laboriosam ad sed ipsa obcaecati modi officia doloremque laborum quam earum exercitationem mollitia. Inventore aperiam praesentium necessitatibus maiores recusandae.</p>
     </>
   );
