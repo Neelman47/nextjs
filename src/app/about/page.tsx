2567c37d9dc952dfe84942fe02287f923d25f277
Neelman47 <neelshah707@gmail.com>
2025-03-17 22:26:43 +0530

    small change

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,11 @@
 import AuroraBG from '@/components/AuroraBG';
+import ATooltip from '@/components/ATooltip';
 
 export default function about() {
     return (
       <>
-      <AuroraBG />
+        <AuroraBG />      
+        <ATooltip />
       </>
     );
   }
